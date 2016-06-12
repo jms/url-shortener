47c46f249713bad2a125d9555fa56496d8c8c8b1
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2016-06-11 23:54:29 -0600

    added setup cfg

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='check-phone-number',
+    version='1.0',
+    url="https://github.com/jms/url-shortener",
+    author="Jeronimo Martinez Sanchez",
+    author_email="jms@rz0r.net",
+    description="URL shortener ",
+    py_modules=['app'],
+    setup_requires=['pytest-runner', ],
+    tests_require=['pytest', ]
+)
