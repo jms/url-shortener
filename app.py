626dab86aa75c42e8e7b5a6fdf68564fa5b0a0ad
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2016-06-11 23:40:16 -0600

    initial commit and basic skeleton

---
+++ app.py
@@ -0,0 +1,35 @@
+from bottle import (
+    route, default_app, get, request, response,
+    template, static_file, jinja2_view, run
+)
+
+
+@route('/')
+@jinja2_view('index.html', template_lookup=['views'])
+def index():
+    return {'title': 'Hello world'}
+
+
+# static files handle
+@route('/images/<filename:re:.*\.png>')
+def send_image(filename):
+    return static_file(filename, root='/static/images', mimetype='image/png')
+
+
+@route('/static/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='/static')
+
+
+@get('/<filename:re:.*\.js>')
+def javascripts(filename):
+    return static_file(filename, root='static/js')
+
+
+@get('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='static/css')
+
+
+# application = default_app()
+run(host='localhost', port=8080, debug=True)
